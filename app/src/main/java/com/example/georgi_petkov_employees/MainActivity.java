899339520047317d0997f0b73a02870f1517f0f8
Ceilar <32637079+Ceilar@users.noreply.github.com>
2023-04-30 11:27:28 +0300

    first commit

---
+++ app/src/main/java/com/example/georgi_petkov_employees/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.georgi_petkov_employees;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
